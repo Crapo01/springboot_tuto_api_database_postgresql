234da9bdc1a2f53bb15fe90082b2d742d84019da
Crapo01 <capusluc@gmail.com>
2024-10-21 11:15:15 +0200

    init

--- Employees API/src/main/java/com/capus/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capus.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
